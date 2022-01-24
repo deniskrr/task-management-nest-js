d527f371eed039d3f4f1845818d6aba9729cc5dc
Denis Crăciunescu <dennis.craciunescu@gmail.com>
2022-01-24 11:03:22 +0200

    Create signUp route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private readonly usersRepository: UsersRepository,
   ) {}
+
+  signUp(createUserDto: CreateUserDto): Promise<void> {
+    return this.usersRepository.createUser(createUserDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<void> {
+    const user = this.create(createUserDto);
+
+    await this.save(user);
+  }
+}
